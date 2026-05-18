130ed97f6a246de6774b7906cc6c6812adc7ea66
YouBeYou <ruben.gadd@outlook.com>
2026-05-18 10:32:43 +0200

    Added CSV saving and Correct Column formatting

--- Python_Script/AfriHate.py
+++ Python_Script/AfriHate.py
@@ -14,10 +14,26 @@ hf_token = ""
 # Read Token From Hugging Face
 login(token=hf_token)
 
-urls = ["afrihate/afrihate/parquet/zul/train"]
-
-
 for split in data_split:
     dataset = load_dataset("afrihate/afrihate", name="zul", split=split)
     df = dataset.filter(lambda label: label['label'] == "Normal")
-    df.to_json(f"{dir_path}afrihate_{split}.jsonl", orient="records", lines=True)
+    
+    # Add Source Columns
+    source_data = ["AfriHate"] * len(df)
+    df = df.add_column("source", source_data)
+
+    # Add MGT Column
+    mgt_data = [True] * len(df)
+    df = df.add_column("MGT", mgt_data)
+
+    # Remove and Rename Columns
+    df = df.remove_columns(["label", 'length'])
+    df = df.rename_column('tweet', 'text')
+
+
+    # Storing Process
+    column_order = ['id', 'source', 'text', 'MGT']
+
+    pandas_df = df.to_pandas()
+    pandas_df = pandas_df[column_order]
+    pandas_df.to_csv(f"{dir_path}afrihate_{split}.csv", index=False)
