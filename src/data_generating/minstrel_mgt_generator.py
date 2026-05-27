f77004891c765a607349c9ab94f72ef66657dd99
Geordan <u22498878@tuks.co.za>
2026-05-27 13:34:49 +0200

    Checkpoint 1

---
+++ src/data_generating/minstrel_mgt_generator.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from minstrel import Minstrel
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+apiKey = os.getenv("MINSTREL_API_KEY")
+
+csv_file_path = "../../data/hgts/hgt_split_2.csv"
